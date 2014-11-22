b830b1786a86922f67be673e8bf776fa0a7fa50a
NastiaR <hotredmulled@gmail.com>
2014-11-22 19:13:41 +0200

    sql

---
+++ sql/create/words.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `words` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `word` text CHARACTER SET utf8 NOT NULL,
+  `descr` text CHARACTER SET utf8 NOT NULL,
+  `trans` text CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
