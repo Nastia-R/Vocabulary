c2bd209c4bc5d1f7f1c6c6a95e72efd3030eb347
NastiaR <hotredmulled@gmail.com>
2015-07-03 16:29:26 +0300

    refactored with 1 point of enter

--- delete_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('autoload.php');
-require_once ('models/acl.php');
-
-$usersObject = new Models\Users;
-mysql_query("SET NAMES utf8");
-require_once('templates/delete_user.phtml');
